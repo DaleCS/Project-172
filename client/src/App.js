21faad52d96c8600fea32c2c7aa1b5255a232337
Dale <Dale.Seen@gmail.com>
2020-04-17 17:22:14 -0700

    003: Landing Page (#7)
    * 001: User Registration
    
    * Created landing page and nav bar
    
    * Created landing page and nav bar
    
    * Fixed register page styling
    
    * Fixed merge conflicts, fixed registration page styling
    
    * Delete Landing_BACKUP_50456.js
    
    * Delete Landing_BASE_50456.js
    
    * Delete Landing_LOCAL_50456.js
    
    * Delete Landing_REMOTE_50456.js
    
    * Co-authored-by: skuchibotla <skuchibotla@gmail.com>
    * Co-authored-by: Sridevi Kuchibotla <skuchibotla1998@gmail.com>
    
    003: Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,14 @@
+// React imports
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Bootstrap/CSS imports
+import styles from './App.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 // Redux Imports
 import { Provider } from "react-redux";
@@ -13,10 +22,23 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
+        <Navbar bg = "dark" expand = "lg" variant = "dark">
+          <Navbar.Toggle aria-controls = "basic-navbar-nav" />
+          <Navbar.Collapse id = "basic-navbar-nav">
+            <Nav className = {[styles.navbar, "mr-auto"].join(' ')}>
+              <Navbar.Brand href = "#home">TODO</Navbar.Brand>
+              <Nav.Link href = "#home">Home</Nav.Link>
+              <Nav.Link href = "#link">Link</Nav.Link>
+            </Nav>
+            <Form inline className = {styles.item}>
+              <Button variant = "outline-success">Login</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
         <Router>
           <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/register" component={Registration} />
+            <Route exact path = "/" component={Landing} />
+            <Route exact path = "/register" component={Registration} />
           </Switch>
         </Router>
       </Provider>
