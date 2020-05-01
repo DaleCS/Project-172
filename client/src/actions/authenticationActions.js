8809331884d1fad6563e1e9600d2495ab670d47d
DaleCS <Dale.Seen@gmail.com>
2020-04-30 21:24:23 -0700

    Fixed some merge problems; Started on Dashboard

--- client/src/actions/authenticationActions.js
+++ client/src/actions/authenticationActions.js
@@ -34,27 +34,7 @@ export const loginUser = async (userCredentials, setLoading, history) => {
     setLoading(false);
     history.push("/dashboard");
   } catch (errRes) {
-    console.log("Error encountered during log in");
-    setLoading(false);
-  }
-};
-
-export const loginUser = async (userCredentials, setLoading, history) => {
-  setLoading(true);
-  try {
-    const body = JSON.stringify(userCredentials);
-
-    const res = await axios.post("/api/user/login", body, config);
-
-    console.log(res.data);
-
-    localStorage.setItem("todotoken", res.data.token);
-    localStorage.setItem("user", JSON.stringify(res.data.user)) 
-
-    setLoading(false);
-    history.push("/dashboard");
-  } catch (errRes) {
-    console.log("Error encountered during log in");
+    console.log(errRes);
     setLoading(false);
   }
 };
