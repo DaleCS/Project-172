018aa44a6c66309fec6eee9cc4e957863c719b27
Tien Nguyen <tienquocnguyen22@gmail.com>
2020-04-30 21:29:26 -0700

    Changing database access back to original

--- client/src/actions/authenticationActions.js
+++ client/src/actions/authenticationActions.js
@@ -38,23 +38,3 @@ export const loginUser = async (userCredentials, setLoading, history) => {
     setLoading(false);
   }
 };
-
-export const loginUser = async (userCredentials, setLoading, history) => {
-  setLoading(true);
-  try {
-    const body = JSON.stringify(userCredentials);
-
-    const res = await axios.post("/api/user/login", body, config);
-
-    console.log(res.data);
-
-    localStorage.setItem("todotoken", res.data.token);
-    localStorage.setItem("user", JSON.stringify(res.data.user)) 
-
-    setLoading(false);
-    history.push("/dashboard");
-  } catch (errRes) {
-    console.log("Error encountered during log in");
-    setLoading(false);
-  }
-};
