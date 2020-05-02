d42187a1599066b829bfed63f8b8b51150351e43
Tien Nguyen <tienquocnguyen22@gmail.com>
2020-05-01 19:34:27 -0700

    Delete update edit (#12)
    * Began work on frontend of user authentication
    
    * Finished User Authentication
    
    * issues with axios call
    
    * dale's commit
    
    * Backend for delete/update/edit
    
    * Changing database access back to original
    
    Co-authored-by: DaleCS <Dale.Seen@gmail.com>

--- server/src/main/java/com/theitcrowd/todo/api/TodoListController.java
+++ server/src/main/java/com/theitcrowd/todo/api/TodoListController.java
@@ -42,10 +42,33 @@ public class TodoListController {
 		return "Saved";
 	}
 
+	@PostMapping(path = "/deleteList")
+	@Transactional
+	public @ResponseBody String deleteList(@RequestParam long todoListID) {
+		listEntryDAO.deleteList(todoListID);
+		todoListDAO.deleteList(todoListID);
+		return "List Deleted";
+	}
+
+	@PostMapping(path = "/updateListTitle")
+	@Transactional
+	public @ResponseBody String updateListTitle(@RequestParam long todoListID, @RequestParam String title) {
+		todoListDAO.updateListTitle(todoListID, title);
+		return "updated title";
+	}
+	
+	@PostMapping(path = "/updateListStatus")
+	@Transactional
+	public @ResponseBody String updateListStatus(@RequestParam long todoListID, @RequestParam String status) {
+		todoListDAO.updateListStatus(todoListID, status);
+		return "updated status";
+	}
+	
 	@GetMapping(path = "/allLists")
 	public @ResponseBody List<TodoList> getAllTodoLists(@RequestParam long user_id){
 		return todoListDAO.findAllLists(user_id);
 	}
+	
 	
 	@PostMapping(path="/addEntry")
 	@Transactional
@@ -53,6 +76,28 @@ public class TodoListController {
 		listEntryDAO.insertEntry(todoEntryForm.getTitle(), todoEntryForm.getListId(), todoEntryForm.getStatus());
 		return "Saved";
 	}
+	
+	@PostMapping(path = "/deleteListEntry")
+	@Transactional
+	public @ResponseBody String deleteListEntry(@RequestParam long entryID) {
+		listEntryDAO.deleteEntry(entryID);
+		return "Entry deleted";
+	}
+	
+	@PostMapping(path = "/updateEntryTitle")
+	@Transactional
+	public @ResponseBody String updateListEntry(@RequestParam long entryID, @RequestParam String title) {
+		listEntryDAO.updateEntryTitle(entryID, title);
+		return "updated title";
+	}
+	
+	@PostMapping(path = "/updateEntryStatus")
+	@Transactional
+	public @ResponseBody String updateEntryStatus(@RequestParam long entryID, @RequestParam String status) {
+		listEntryDAO.updateEntryStatus(entryID, status);
+		return "updated status";
+	}
+	
 	
 	@GetMapping(path = "/allEntries")
 	public List<TodoListEntry> getAllTodoListEntries(@RequestParam long todoListID){
