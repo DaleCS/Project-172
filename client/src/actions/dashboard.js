56047234a7676859800d7fa01e0eeea1b928e43b
DaleCS <Dale.Seen@gmail.com>
2020-05-01 17:21:24 -0700

    Added Dashboard Component; Adding Todo Lists and entries

--- client/src/actions/dashboard.js
+++ client/src/actions/dashboard.js
@@ -1,30 +1,121 @@
 import axios from "axios";
 
-const token = localStorage.getItem("todotoken")
+const userData = JSON.parse(localStorage.getItem("todotoken"));
 const config = {
-    headers: {
-      "content-type": "application/json",
-      Accept: 'application/json',
-      'Authorization': `Bearer ${token}`
-    },
-  };
+  headers: {
+    "content-type": "application/json",
+    Accept: "application/json",
+    Authorization: `Bearer ${userData.token}`,
+  },
+};
 
-  export const AllLists = async ()=>{
-    const user = JSON.parse(localStorage.getItem("user"))
-    const lists = await axios.get('/api/todolists/allLists', {
-      params: {
-      user_id :  user.userId
-    }}, config)
-    return lists
+export const fetchTodoLists = async (setLoading) => {
+  setLoading(true);
+  try {
+    const res = await axios.get(
+      `/api/todolists/allLists?user_id=${userData.user.userId}`,
+      config
+    );
+
+    // Transform the data into a format that the component can better render
+    const transformedTodoLists = res.data.map((todoList) => {
+      const {
+        creationDate,
+        description,
+        modification_date,
+        status,
+        title,
+        todo_list_id,
+      } = todoList;
+
+      return {
+        todoListId: todo_list_id,
+        title,
+        description,
+        status,
+        creationDate,
+        modificationDate: modification_date,
+      };
+    });
+
+    setLoading(false);
+    return transformedTodoLists;
+  } catch (errRes) {
+    console.log(errRes);
+    setLoading(false);
+    return [];
   }
+};
 
-  export const addTodoList = async () => {
-    //const body = JSON.stringify(list);
-    //await axios.post("/api/todolists/addList", body, config);
-    // history.pushState("/dashboard")
-  };
-  
-  export const addTodoListItem = async () => {
+export const fetchTodoListEntries = async (todoListId, setLoading) => {
+  setLoading(true);
+  try {
+    const res = await axios.get(
+      `/api/todolists/allEntries?todoListID=${todoListId}`,
+      config
+    );
 
-  };
-  
+    // Transform the data into a format that the coponent can better render
+    const transformedTodoListsEntries = res.data.map((entry) => {
+      const { description, status, title, entry_id } = entry;
+      let transformedStatus = false;
+
+      switch (status.toLowerCase()) {
+        case "planned": {
+          transformedStatus = false;
+          break;
+        }
+        case "completed": {
+          transformedStatus = true;
+          break;
+        }
+        default: {
+        }
+      }
+      return {
+        entryId: entry_id,
+        title,
+        status: transformedStatus,
+        description,
+      };
+    });
+
+    setLoading(false);
+    return transformedTodoListsEntries;
+  } catch (errRes) {
+    console.log(errRes);
+    setLoading(false);
+    return [];
+  }
+};
+
+export const addTodoList = async (body, setLoading, setTodoLists) => {
+  setLoading(true);
+  try {
+    body.user_id = userData.user.userId;
+    await axios.post(`/api/todolists/addList`, body, config);
+    setTodoLists(await fetchTodoLists(setLoading));
+    setLoading(false);
+  } catch (errRes) {
+    console.log(errRes);
+    setLoading(false);
+  }
+};
+
+export const addTodoListEntry = async (
+  body,
+  setLoading,
+  setTodoListEntries
+) => {
+  setLoading(true);
+  try {
+    await axios.post(`/api/todolists/addEntry`, body, config);
+    setTodoListEntries(
+      await fetchTodoListEntries(body.todo_list_id, setLoading)
+    );
+    setLoading(false);
+  } catch (errRes) {
+    console.log(errRes);
+    setLoading(false);
+  }
+};
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,9 +1,31 @@
 import React, { useState, useEffect } from "react";
 
+import {
+  fetchTodoLists,
+  fetchTodoListEntries,
+  addTodoList,
+  addTodoListEntry,
+} from "../../actions/dashboard";
+
 import TodoList from "./TodoList";
 import TodoListItem from "./TodoListItem";
 
-import { makeStyles, Grid, Paper, Button } from "@material-ui/core";
+import {
+  makeStyles,
+  Grid,
+  Paper,
+  Button,
+  CircularProgress,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  TextField,
+  DialogActions,
+  IconButton,
+} from "@material-ui/core";
+
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import AddIcon from "@material-ui/icons/Add";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -44,78 +66,67 @@ const useStyles = makeStyles((theme) => ({
   mt8: {
     marginTop: "8px",
   },
+  addTodoListBtn: {
+    color: "#2ecc71",
+  },
+  AddTodoListDialog: {
+    minWidth: "420px",
+  },
 }));
 
 const Dashboard = (props) => {
-  const [highlightedTodoListIndex, setHighlightedTodoListIndex] = useState(-1);
   const [todoLists, setTodoLists] = useState([]);
+  const [todoListEntries, setTodoListEntries] = useState([]);
+  const [isLoadingTodoLists, setLoadingTodoLists] = useState(false);
+  const [isLoadingTodoListEntries, setLoadingTodoListEntries] = useState(false);
+
+  const [openTodoListDialog, setTodoListDialog] = useState(false);
+  const [newTodoListField, setNewTodoListField] = useState("");
+
+  const [addTodoListEntryField, setTodoListEntryField] = useState("");
+
+  const [currentTodoListId, setCurrentTodoListId] = useState(-1);
 
   useEffect(() => {
-    const dummyData = [
-      {
-        todoListId: 1,
-        title: "Chores",
-        description: "",
-        status: "Planned",
-        creationDate: "",
-        modificationDate: "",
-        todoListEntries: [
-          {
-            entryId: 1,
-            title: "Wash the dishes",
-            status: false,
-            description: "",
-          },
-          {
-            entryId: 2,
-            title: "Sweep the floor",
-            status: false,
-            description: "",
-          },
-        ],
-      },
-      {
-        todoListId: 1,
-        title: "Homework",
-        description: "",
-        status: "Planned",
-        creationDate: "",
-        modificationDate: "",
-        todoListEntries: [
-          {
-            entryId: 1,
-            title: "CMPE172",
-            status: false,
-            description: "",
-          },
-          {
-            entryId: 2,
-            title: "CMPE187",
-            status: false,
-            description: "",
-          },
-        ],
-      },
-    ];
+    const fetchUserTodoLists = async () => {
+      setTodoLists(await fetchTodoLists(setLoadingTodoLists));
+    };
 
-    setTodoLists(dummyData);
+    fetchUserTodoLists();
   }, []);
 
-  const handleOnClickList = (index) => {
-    setHighlightedTodoListIndex(index);
+  const classes = useStyles(); // CSS Styling for UI elements
+
+  /**
+   * Handles clicks on the a todo list
+   * @param {number} index
+   */
+  const handleOnClickList = async (index) => {
+    if (todoLists[index].todoListId) {
+      setCurrentTodoListId(todoLists[index].todoListId);
+      setTodoListEntries(
+        await fetchTodoListEntries(
+          todoLists[index].todoListId,
+          setLoadingTodoListEntries
+        )
+      );
+    }
   };
 
+  /**
+   * Handles clicks on an entry
+   * @param {number} index
+   * @param {boolean} value
+   */
   const handleOnClickEntry = (index, value) => {
-    const todoListsClone = [...todoLists];
-    todoListsClone[highlightedTodoListIndex].todoListEntries[
-      index
-    ].status = value;
-
-    setTodoLists(todoListsClone);
+    const todoListEntriesClone = [...todoListEntries];
+    todoListEntriesClone[index].status = value;
+    setTodoListEntries(todoListEntriesClone);
   };
 
-  const classes = useStyles();
-
+  /**
+   * Maps through todolists to render them
+   */
   const todoListsMap = todoLists.map((list, index) => {
     return (
       <TodoList
@@ -127,23 +138,67 @@ const Dashboard = (props) => {
     );
   });
 
-  const todoListEntriesMap = () => {
-    if (highlightedTodoListIndex > -1) {
-      return todoLists[highlightedTodoListIndex].todoListEntries.map(
-        (entry, index) => {
-          return (
-            <TodoListItem
-              entry={entry}
-              handleOnClickEntry={handleOnClickEntry}
-              key={index}
-              index={index}
-            />
-          );
-        }
+  /**
+   * Maps through entries to render them
+   */
+  const todoListEntriesMap = todoListEntries.map((entry, index) => {
+    return (
+      <TodoListItem
+        entry={entry}
+        index={index}
+        handleOnClickEntry={handleOnClickEntry}
+        key={index}
+      />
+    );
+  });
+
+  const handleNewTodoListFieldChange = (event) => {
+    setNewTodoListField(event.target.value);
+  };
+
+  const handleCloseTodoListDialog = () => {
+    setTodoListDialog(false);
+  };
+
+  const handleOpenTodoListDialog = (event) => {
+    event.preventDefault();
+    setTodoListDialog(true);
+  };
+
+  const handleCreateTodoList = (event) => {
+    event.preventDefault();
+
+    const newTodoList = {
+      title: newTodoListField,
+      description: "",
+      status: "planned",
+    };
+
+    addTodoList(newTodoList, setLoadingTodoLists, setTodoLists);
+    setTodoListDialog(false);
+  };
+
+  const handleAddTodoListEntry = (event) => {
+    if (currentTodoListId > -1) {
+      event.preventDefault();
+
+      const newTodoListEntry = {
+        title: addTodoListEntryField,
+        todo_list_id: currentTodoListId,
+        description: "",
+        status: "planned",
+      };
+
+      addTodoListEntry(
+        newTodoListEntry,
+        setLoadingTodoListEntries,
+        setTodoListEntries
       );
-    } else {
-      return <div />;
     }
+  };
+
+  const handleNewEntryFieldChange = (event) => {
+    setTodoListEntryField(event.target.value);
   };
 
   return (
@@ -166,12 +221,38 @@ const Dashboard = (props) => {
             <Grid item xs={4}>
               <Paper variant="outlined" square className={classes.h100}>
                 <Grid container direction="column" justify="flex-start">
-                  {todoListsMap}
-                  <Grid item alignSelf="center" className={classes.mt8}>
-                    <Button variant="contained" color="primary">
-                      Create New Todo List
-                    </Button>
-                  </Grid>
+                  {isLoadingTodoLists === false ? (
+                    todoListsMap
+                  ) : (
+                    <Grid item>
+                      <CircularProgress />
+                    </Grid>
+                  )}
+                  {isLoadingTodoLists === false ? (
+                    <Grid item className={classes.mt8}>
+                      <Grid
+                        container
+                        direction="row"
+                        alignItems="center"
+                        justify="center"
+                        spacing={1}
+                      >
+                        <Grid item>
+                          <IconButton
+                            size="medium"
+                            onClick={handleOpenTodoListDialog}
+                          >
+                            <AddCircleIcon
+                              className={classes.addTodoListBtn}
+                              fontSize="large"
+                            />
+                          </IconButton>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  ) : (
+                    <div />
+                  )}
                 </Grid>
               </Paper>
             </Grid>
@@ -182,13 +263,73 @@ const Dashboard = (props) => {
                 className={classes.todoListEntries}
               >
                 <Grid container direction="column" justify="flex-start">
-                  {todoListEntriesMap()}
+                  {isLoadingTodoListEntries === false ? (
+                    todoListEntriesMap
+                  ) : (
+                    <Grid item>
+                      <CircularProgress />
+                    </Grid>
+                  )}
+                  {currentTodoListId > -1 &&
+                  isLoadingTodoListEntries === false ? (
+                    <Grid item xs={12}>
+                      <Grid
+                        container
+                        direction="row"
+                        justify="flex-start"
+                        alignItems="center"
+                        spacing={1}
+                      >
+                        <Grid item xs={11}>
+                          <TextField
+                            margin="none"
+                            label="Add A New Entry"
+                            size="small"
+                            fullWidth
+                            onChange={handleNewEntryFieldChange}
+                          />
+                        </Grid>
+                        <Grid item xs={1}>
+                          <IconButton onClick={handleAddTodoListEntry}>
+                            <AddIcon />
+                          </IconButton>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  ) : (
+                    <div />
+                  )}
                 </Grid>
               </Paper>
             </Grid>
           </Grid>
         </Paper>
       </Grid>
+      <Dialog
+        open={openTodoListDialog}
+        onClose={handleCloseTodoListDialog}
+        className={classes.AddTodoListDialog}
+      >
+        <DialogTitle>Add A New Todo List</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Title"
+            type="email"
+            fullWidth
+            onChange={handleNewTodoListFieldChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseTodoListDialog} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleCreateTodoList} color="primary">
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Grid>
   );
 };
